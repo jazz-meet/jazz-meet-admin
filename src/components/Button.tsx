ea9e0bc7157beecaa67b70d81abf627e6e4bbd82
jsh3418 <albert5428@gmail.com>
2023-11-29 14:12:53 +0900

    feat: 버튼 컴포넌트 min-width, min-height 설정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
-export const StyledButton = styled.button`
+export const Button = styled.button`
+  min-width: 37.5px;
+  min-height: 37.5px;
   padding: 10px;
   border-radius: 8px;
   border: 1px solid #000000;
