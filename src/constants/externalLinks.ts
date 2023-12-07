1f75f4555db1b36f6d439c02288e3d5c10bcfc96
jsh3418 <albert5428@gmail.com>
2023-12-07 16:17:22 +0900

    fix: 불필요한 상수 삭제

--- src/constants/externalLinks.ts
+++
@@ -1,7 +0,0 @@
-export const EXTERNAL_LINKS = [
-  { type: 'naverMap', text: '네이버 지도' },
-  { type: 'instagram', text: '인스타그램' },
-  { type: 'official', text: '공식 홈페이지' },
-  { type: 'etc', text: '기타' },
-  { type: 'reservation', text: '예약 링크' },
-];
