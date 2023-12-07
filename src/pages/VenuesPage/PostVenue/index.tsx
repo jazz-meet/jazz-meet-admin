fd4d05e62b3150c12951c04de912c69785a32133
jsh3418 <albert5428@gmail.com>
2023-12-07 16:16:04 +0900

    fix: 불필요한 헤더 삭제

--- src/pages/VenuesPage/PostVenue/index.tsx
+++ src/pages/VenuesPage/PostVenue/index.tsx
@@ -9,7 +9,6 @@ export const PostVenue: React.FC = () => {
     initializeEditForm: state.initializeEditForm,
   }));
   const { venueId } = useParams();
-  const isEdit = !!venueId;
 
   useEffect(() => {
     if (venueId) {
@@ -20,9 +19,6 @@ export const PostVenue: React.FC = () => {
 
   return (
     <StyledPostVenue>
-      <StyledHeader>{`공연장 > 공연장 ${
-        isEdit ? '수정' : '생성'
-      }`}</StyledHeader>
       <PostVenueForm />
     </StyledPostVenue>
   );
@@ -30,10 +26,4 @@ export const PostVenue: React.FC = () => {
 
 const StyledPostVenue = styled.div`
   width: 100%;
-`;
-
-const StyledHeader = styled.div`
-  border-bottom: 1px solid #000000;
-  font-size: 24px;
-  padding: 20px;
 `;
