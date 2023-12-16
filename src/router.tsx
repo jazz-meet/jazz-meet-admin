63541bf57604986fd5c980c91dec9d5ba1038c12
Jay <chlwodnjs34@gmail.com>
2023-12-16 14:14:02 +0900

    refactor : 공연 관련 router 추가

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,8 @@ import { PATH } from './constants/path';
 import { BaseLayout } from './layouts/BaseLayout';
 import { InquiriesPage } from './pages/InquiriesPage';
 import { ShowsPage } from './pages/ShowsPage';
+import { PostShow } from './pages/ShowsPage/PostShow';
+import { ShowDetail } from './pages/ShowsPage/ShowDetail';
 import { VenuesPage } from './pages/VenuesPage';
 import { PostVenue } from './pages/VenuesPage/PostVenue';
 import { VenueDetail } from './pages/VenuesPage/VenueDetail';
@@ -20,11 +22,13 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={PATH.HOME} element={<BaseLayout />}>
       <Route path={PATH.VENUES} element={<VenuesPage />} />
-      <Route path={PATH.VENUES + '/:venueId'} element={<VenueDetail />} />
+      <Route path={PATH.VENUES_DETAIL} element={<VenueDetail />} />
       <Route path={PATH.VENUES_POST} element={<PostVenue />} />
-      <Route path={PATH.VENUES_EDIT + '/:venueId'} element={<PostVenue />} />
+      <Route path={PATH.VENUES_EDIT} element={<PostVenue />} />
 
       <Route path={PATH.SHOWS} element={<ShowsPage />} />
+      <Route path={PATH.SHOWS_DETAIL} element={<ShowDetail />} />
+      <Route path={PATH.SHOWS_POST} element={<PostShow />} />
 
       <Route path={PATH.INQUIRIES} element={<InquiriesPage />} />
     </Route>,
