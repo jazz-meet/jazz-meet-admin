f2dd171db4f3f86daff4e7b1df62acf8289358d1
jsh3418 <albert5428@gmail.com>
2024-03-09 20:57:07 +0900

    feat: Add fetchDataWithToken function to fetch data with authorization token

--- src/apis/fetchData.ts
+++ src/apis/fetchData.ts
@@ -1,5 +1,24 @@
 import { BASE_URL } from '~/constants/envVariables';
+import { getTokenFromSessionStorage } from '~/utils/storage';
 
 export const fetchData = async (path: string, options?: RequestInit) => {
   return fetch(BASE_URL + path, options);
+};
+
+export const fetchDataWithToken = async (
+  path: string,
+  options?: RequestInit,
+) => {
+  const token = getTokenFromSessionStorage();
+  if (!token) {
+    throw new Error('Token not found');
+  }
+
+  return fetchData(path, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
